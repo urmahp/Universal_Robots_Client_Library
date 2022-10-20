5afe1b5a09bf559f45bf83b5833f1b6275276b56
urrsk <41109954+urrsk@users.noreply.github.com>
2022-10-20 13:38:58 +0200

    Adding function to interact with the robot through the dashboard
    Adding short function to make it easier to:
    - Power off
    - Power on
    - Brake release
    - Load a program
    - Play
    - Pause
    - Stop
    - Close popup
    - Close safety popup
    - Restart safety
    - Unlock protective stop
    - Shutdown
    
    An example is also added

--- include/ur_client_library/ur/dashboard_client.h
+++ include/ur_client_library/ur/dashboard_client.h
@@ -78,6 +78,99 @@ public:
    */
   std::string sendAndReceive(const std::string& command);
 
+  /*!
+   * @brief Sends command and compare it with the expected answer
+   *
+   * @param command Command that will be sent to the server. It is important, that the command sent is finished with a
+   * '\n' (newline) so it will be processed by the server.
+   * @param expected Expected replay
+   *
+   * @return True if the reply to the command is as expected
+   */
+  bool sendRequest(const std::string& command, const std::string& expected);
+
+  /*! @brief Sends a command and wait until it returns the expected answer
+   *  @param command Command that will be sent to the server
+   *  @param expected Expected replay
+   *  @param timeout Timeout time in seconds
+   *  @return True if the reply was as expected within the timeout time
+   */
+  bool waitForReply(const std::string& command, const std::string& expected, double timeout = 30.0);
+
+  /*!
+   * @brief Send Power off command
+   * @return True succeeded
+   */
+  bool commandPowerOff();
+
+  /*!
+   * @brief Send Power on command
+   * @param timeout Timeout in seconds
+   * @return True succeeded
+   */
+  bool commandPowerOn(unsigned int timeout = 1200);
+
+  /*!
+   * @brief Send Breake release command
+   * @return True succeeded
+   */
+  bool commandBreakeRelease();
+
+  /*!
+   * @brief Send Load program command
+   * @param program_file_name The urp program file name with the urp extension
+   * @return True succeeded
+   */
+  bool commandLoadProgram(const std::string& program_file_name);
+
+  /*!
+   * @brief Send Play program command
+   * @return True succeeded
+   */
+  bool commandPlay();
+
+  /*!
+   * @brief Send Pause program command
+   * @return True succeeded
+   */
+  bool commandPause();
+
+  /*!
+   * @brief Send Stop program command
+   * @return True succeeded
+   */
+  bool commandStop();
+
+  /*!
+   * @brief Send Close popup command
+   * @return True succeeded
+   */
+  bool commandClosePopup();
+
+  /*!
+   * @brief Send Close safety popup command
+   * @return True succeeded
+   */
+  bool commandCloseSafetyPopup();
+
+  /*!
+   * @brief Send Restart Safety command
+   * @return True succeeded
+   */
+  bool commandRestartSafety();
+
+  /*!
+   * @brief Send Unlock Protective stop popup command
+   * @return True succeeded
+   */
+  bool commandUnlockProtectiveStop();
+
+  /*!
+   * @brief Send Shutdown command
+   * @return True succeeded
+   */
+  bool commandShutdown();
+
 protected:
   virtual bool open(int socket_fd, struct sockaddr* address, size_t address_len)
   {
@@ -87,11 +180,7 @@ protected:
 private:
   bool send(const std::string& text);
   std::string read();
-
-  void rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
-  {
-    str.erase(str.find_last_not_of(chars) + 1);
-  }
+  void rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ");
 
   std::string host_;
   int port_;
