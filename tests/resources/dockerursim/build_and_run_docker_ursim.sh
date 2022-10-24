2ef439043c92c3ab6669077e18109991c299e009
urmahp <mahp@universal-robots.com>
2022-10-24 16:06:47 +0200

    Fix formatting and make industrial ci depend on dockerhub simulator

--- tests/resources/dockerursim/build_and_run_docker_ursim.sh
+++ tests/resources/dockerursim/build_and_run_docker_ursim.sh
@@ -3,8 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 URSIM_DOCKER_IMAGE=${1}
 PROGRAM_FOLDER=${2}
-echo "PROGRAM_FOLDER: ${PROGRAM_FOLDER}"
-echo "URSIM_DOCKER_IMAGE: ${URSIM_DOCKER_IMAGE}"
 
 docker network create --subnet=192.168.56.0/24 static_test_net
 
@@ -15,7 +13,7 @@ docker run --rm -it -d \
   -p 8080:8080 \
   -p 29999:29999 \
   -p 30001-30004:30001-30004 \
-  -v "${DIR}/{$PROGRAM_FOLDER}":/ursim/programs \
+  -v "${DIR}/${PROGRAM_FOLDER}":/ursim/programs \
   --privileged \
   --cpus=1 \
   ${URSIM_DOCKER_IMAGE}
